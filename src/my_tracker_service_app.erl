9a73e910a72c600abc1012701ca5c9a5e7e6f67c
Carter <carterhwms@gmail.com>
2024-06-11 13:16:12 -0600

    copied  from github hello

--- src/my_tracker_service_app.erl
+++ src/my_tracker_service_app.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%% @doc my_tracker_service public API
+%% @doc hello public API
 %% @end
 %%%-------------------------------------------------------------------
 
@@ -10,27 +10,28 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-        %{"/", cowboy_static, {priv_file, db_access, "static/index.html"}},
-            {"/", default_page_h, []},
-            {"/package_transferred", package_transfer_page, []}
+    
+        Dispatch = cowboy_router:compile([
+            {'_', [
+                {"/", default_page_h, []},
+                {"/package_transferred", package_transfer_page, []}
+                
+            ]}
+        ]),
 
+    	PrivDir = code:priv_dir(my_tracker_service),
+    	%tls stands for transport layer security
+          {ok,_} = cowboy:start_tls(https_listener, [
+                  		{port, 443},
+				{certfile, PrivDir ++ "/ssl/fullchain.pem"},
+				{keyfile, PrivDir ++ "/ssl/privkey.pem"}
+              		], #{env => #{dispatch => Dispatch}}),
 
+        hello_sup:start_link().
 
-        ]}
-    ]),
-
-    PrivDir = code:priv_dir(my_tracker_service),
-    %tls stands for transport layer security
-        {ok,_} = cowboy:start_tls(https_listener, [
-                                    {port, 443},
-                                    {certfile, PrivDir ++ "/ssl/fullchain.pem"},
-                                    {keyfile, PrivDir ++ "/ssl/privkey.pem"}],
-                                    #{env => #{dispatch => Dispatch}}),
-
-        my_tracker_service_sup:start_link().
 stop(_State) ->
-    ok.
+    ok.                                                                                     
+
+%% internal functions
 
 %% internal functions
