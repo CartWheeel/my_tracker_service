89a1ae2cf1eb280a9ca08f3bc7abe8a0e999db9c
Carter <carterhwms@gmail.com>
2024-06-11 13:01:20 -0600

    removed typeo

--- src/my_tracker_service_app.erl
+++ src/my_tracker_service_app.erl
@@ -10,18 +10,20 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-        Dispatch = cowboy_router:compile([
-            {'_', [
-                {"/", default_page_h, []},
-                {"/package_transferred", package_transfer_page, []}
+    Dispatch = cowboy_router:compile([
+        {'_', [
+        %{"/", cowboy_static, {priv_file, db_access, "static/index.html"}},
+            {"/", default_page_h, []},
+            {"/package_transferred", package_transfer_page, []}
 
-            ]}
-        ]),
 
-        PrivDir = code:priv_dir(my_tracker_service),
-        %tls stands for transport layer security
-        
-        {ok, _} = cowboy:start_tls(https_listener, [
+
+        ]}
+    ]),
+
+    PrivDir = code:priv_dir(my_tracker_service),
+    %tls stands for transport layer security
+        {ok,_} = cowboy:start_tls(https_listener, [
                                     {port, 443},
                                     {certfile, PrivDir ++ "/ssl/fullchain.pem"},
                                     {keyfile, PrivDir ++ "/ssl/privkey.pem"}],
