db20095de24d944a4986eb58d4cc6b1db47df60e
Carter <carterhwms@gmail.com>
2024-06-26 10:41:40 -0600

    words

--- src/default_page_h.erl
+++ src/default_page_h.erl
@@ -6,5 +6,5 @@
 init(Req0, Opts) ->
         Req = cowboy_req:reply(200, #{
                 <<"content-type">> => <<"text/plain">>
-        }, "HELLO WORLD!", Req0),
+        }, "HELLO WORLD! Did this work?", Req0),
         {ok, Req, Opts}.
