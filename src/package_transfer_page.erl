9d73d32a74fc4cea9f06aa683543772605709bcb
Carter <carterhwms@gmail.com>
2024-07-01 11:16:15 -0600

    changed back to original

--- src/package_transfer_page.erl
+++ src/package_transfer_page.erl
@@ -1,27 +1,29 @@
 -module(package_transfer_page).
+
 -export([init/2]).
 
 init(Req0, Opts) ->
-    {ok, Data, _} = cowboy_req:read_body(Req0),
-    case jsx:decode(Data) of
-        {ok, DecodedData} ->
-            case extract_ids(DecodedData) of
-                {ok, Package_ID, Location_ID} ->
-                    Result = erpc:call('deliveryman@bl.wheresmypackages.com', business_logic, put_package, [Package_ID, Location_ID]),
-                    Encoded_message = jsx:encode(Result),
-                    Response = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Encoded_message, Req0),
-                    {ok, Response, Opts};
-                {error, Reason} ->
-                    Response = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, jsx:encode(#{error => "Invalid data format", reason => Reason}), Req0),
-                    {ok, Response, Opts}
-            end;
-        {error, Reason} ->
-            Response = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, jsx:encode(#{error => "Invalid JSON data", reason => Reason}), Req0),
-            {ok, Response, Opts}
-    end.
+        {ok,Data,_} = cowboy_req:read_body(Req0),
+        % it is expected that the data consists of one quoted-string name
+        % in an array.
+        
+        [{<<"Package_ID">>,Package_ID},{<<"Location_ID">>,Location_ID}]  = jsx:decode(Data),
+        % test [<<"35">>,<<"14">>] = [Package_ID,Location_ID],
+        Result = erpc:call('deliveryman@bl.wheresmypackages.com', business_logic, put_package, [Package_ID,Location_ID]), 
+        
+        %get_friends_server:get_friends_of(Package_ID) of
+                %         {error,notfound} -> "no such person";
+                %         Friends -> Friends
+                % end,
+                
+        Encoded_message = jsx:encode(Result),
+        %io:format("~p~n",[get_friends_server:get_friends_of(Name)]),
 
-%% Helper function to extract Package_ID and Location_ID
-extract_ids([{<<"Package_ID">>, Package_ID}, {<<"Location_ID">>, Location_ID}]) ->
-    {ok, Package_ID, Location_ID};
-extract_ids(_) ->
-    {error, "Package_ID and Location_ID not found in JSON data."}.
+        Response = cowboy_req:reply(200,
+                #{<<"content-type">> => <<"text/json">>},
+                Encoded_message, Req0),
+        {ok, Response, Opts}.
+
+
+
+
